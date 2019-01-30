9e1444c6fd4f79395d78f9559830556f42ecea1a
Kurt Bauer <kurtbauer55@protonmail.com>
2019-01-30 14:23:54 -0500

    decided on ansii symbol for title tag, added initial about me blurb

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,10 +5,6 @@ import IntroCard from "../components/IntroCard"
 const IndexPage = () => (
   <div>
     <IntroCard/>
-    <h1 className="fw5 sans-serif">Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
   </div>
 )
 
