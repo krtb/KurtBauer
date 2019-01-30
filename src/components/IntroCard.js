0924127073ab4aaa1c2df1008c96b33b53ebdc81
Kurt Bauer <kurtbauer55@protonmail.com>
2019-01-30 16:34:33 -0500

    modified README

--- src/components/IntroCard.js
+++ src/components/IntroCard.js
@@ -3,11 +3,11 @@ import React, { Component } from 'react';
 class IntroCard extends Component {
     render(){
     return <article className="center mw5 mw6-ns hidden ba mv4">
-        <h1 className="f4 bg-near-black white mv0 pv2 ph3">//</h1>
+        <h1 className="f4 bg-near-black white mv0 pv2 ph3">//info_about/me.txt</h1>
         <div className="pa3 bt">
           <p className="f6 f5-ns lh-copy measure mv0">
             Kurt Bauer is latinx, a full-stack software engineer, web & mobile designer,
-            writer, artist, photographer, and probably still anxious about cleaning his inbox.  
+            writer, artist, photographer, progressive activist, and probably still anxious about cleaning his inbox.  
           </p>
         </div>
       </article>}
