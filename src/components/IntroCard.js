afd9e723add8827cb5f7ba3c426538d1a70218fc
Kurt Bauer <kurtbauer55@protonmail.com>
2019-01-31 15:45:48 -0500

    fixed jsx and missing link to tachyons CDN in index.html

--- src/components/IntroCard.js
+++ src/components/IntroCard.js
@@ -1,17 +1,19 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class IntroCard extends Component {
-    render(){
-    return <article className="center mw5 mw6-ns hidden ba mv4">
-        <h1 className="f4 bg-near-black white mv0 pv2 ph3">//info_about/me.txt</h1>
-        <div className="pa3 bt">
-          <p className="f6 f5-ns lh-copy measure mv0">
-            Kurt Bauer is latinx, a full-stack software engineer, web & mobile designer,
-            writer, artist, photographer, progressive activist,
-            and I will always have a faster site than you.
-          </p>
-        </div>
-      </article>}
+const IntroCard = () => {
+  let title = "//info_about/me.txt"
+  let blurb = "Kurt Bauer is latinx, a full-stack software engineer, web & mobile designer, writer, artist, photographer, progressive activist, and I will always have a faster site than you."
+
+  return (
+      <article className="center mw5 mw6-ns hidden ba mv4">
+          <h1 className="f4 bg-near-black white mv0 pv2 ph3">{title}</h1>
+          <div className="pa3 bt">
+            <p className="f6 f5-ns lh-copy measure mv0">
+              {blurb}
+            </p>
+          </div>
+        </article>
+  )
 }
 
 export default IntroCard;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Link } from "gatsby"
+// import { Link } from "gatsby"
 import IntroCard from "../components/IntroCard"
 
 const IndexPage = () => (
