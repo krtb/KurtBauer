010967541a32bb04f96287e7f523970fa2845f75
Kurt Bauer <kurtbauer55@protonmail.com>
2019-02-19 20:01:00 -0500

    adding bottom grid to navigate to projects

--- src/homepage/components/first.js
+++ src/homepage/components/first.js
@@ -3,20 +3,29 @@ import React, { Component } from 'react'
 export default class first extends Component {
   render() {
     return (
-        <article>
-            <header class="bg-gold sans-serif">
-                <div class="mw9 center pa4 pt5-ns ph7-l">
-                    <h3 class="f2 f1-m f-headline-l measure-narrow lh-title mv0">
-                        <span class="bg-black-90 lh-copy white pa1 tracked-tight">
-                            Hi, I'm Kurt Bauer.
-                        </span>
-                        <span class="bg-black-90 lh-copy white pa1 tracked-tight">
-                            I'm a software engineer and mobile app developer.
-                        </span>
-                    </h3>
-                </div>
-            </header>
-        </article>
+        <main>
+  <section class="cf mt5 pv5 bt b--black-05 ph6-l">
+    <h1 class="tc f5 ttu fw6 tracked mb4 avenir">Other Projects</h1>
+    <a href="#0" class="fl w-third w-25-ns border-box overflow-hidden ba bw2 white" title="">
+      <div class="grow cover bg-center pv5 pv6-l" style={{backgroundImage:'url(https://s3-us-west-2.amazonaws.com/prnt/hw-080411-cargo_960.jpg)'}}></div>
+    </a>
+    <a href="#0" class="fl w-third w-25-ns border-box overflow-hidden  ba bw2 white" title="">
+      <div class="grow cover bg-top pv5 pv6-l" style={{backgroundImage:'url(https://s3-us-west-2.amazonaws.com/prnt/hwspringtour-cargo_960-1.jpg)'}}></div>
+    </a>
+    <a href="#0" class="fl w-third w-25-ns border-box overflow-hidden ba bw2 white" title="">
+      <div class="grow cover bg-top pv5 pv6-l" style={{backgroundImage:'url(https://s3-us-west-2.amazonaws.com/prnt/cc010611.s_960.jpg)'}}></div>
+'    </a>
+    <a href="#0" class="fl w-100 w-25-ns border-box overflow-hidden ba bw2 white" title="">
+      <div class="grow cover bg-top pv5 pv6-l" style={{backgroundImage:'url(https://s3-us-west-2.amazonaws.com/prnt/adam-stern-031209_960-2.jpg)'}}></div>
+    </a>
+    <a href="#0" class="fl w-50 border-box overflow-hidden ba bw2 white" title="">
+      <div class="grow cover bg-center pv5 pv7-l" style={{backgroundImage:'url(https://s3-us-west-2.amazonaws.com/prnt/zh170311.4.cargo_960.jpg)'}}></div>
+    </a>
+    <a href="#0" class="fl w-50 border-box overflow-hidden ba bw2 white" title="">
+      <div class="grow cover bg-center pv5 pv7-l" style={{backgroundImage:'url(https://s3-us-west-2.amazonaws.com/prnt/hw090911_960.jpg)'}}></div>
+  '  </a>
+  </section>
+</main>
     )
   }
 }
