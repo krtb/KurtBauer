d889e88a57b3e6cf1d57fe8220a6fca0e0934b97
Kurt Bauer <kurtbauer55@protonmail.com>
2019-03-24 08:09:32 -0400

    delete empty line

--- src/homepage/mainBlurb.js
+++ src/homepage/mainBlurb.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import 'tachyons'
 
-
 export default () => (
 <article className="center mw5 mw6-ns hidden ba mv4">
     <h1 className="sans-serif f4 bg-near-black white mv0 pv2 ph3">
