fee7e9ca805a9a2a6bb8a9d165f835a8a8f93674
Kurt Bauer <kurtbauer55@protonmail.com>
2019-05-31 16:35:57 -0400

    try centered email text div

--- src/homepage/ContactMe.js
+++ src/homepage/ContactMe.js
@@ -3,11 +3,20 @@ import 'tachyons'
 
 const ContactMe = () => {
     return(
+            <React.Fragment>
+            <header class="tc ph4 sans-serif">
+                <h1 class="f3 f2-m f1-l fw2 black-90 mv3">
+                    kurtbauer55@protonmail.com
+  </h1>
+                <h2 class="f5 f4-m f3-l fw2 black-50 mt0 lh-copy">
+                    Let's talk
+  </h2>
+            </header>
             <footer class="pv4 ph3 tc sans-serif">
-            <div class="ph3 ph4-ns pv6 bt b--black-10 black-70">
+            {/* <div class="ph3 ph4-ns pv6 bt b--black-10 black-70">
                 <a href="mailto:" class="link b f3 f2-ns dim black-70 lh-solid">kurtbauer55@protonmail.com</a>
                 <p class="f6 db b ttu lh-solid">Let's talk</p>
-            </div>
+            </div> */}
         <a class="link near-black hover-silver dib mh3 tc" href="https://github.com/mrmrs" title="GitHub">
             <svg class="dib h2 w2" fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill-rule="evenodd" clip-rule="evenodd" stroke-linejoin="round" stroke-miterlimit="1.414"><path d="M8 0C3.58 0 0 3.582 0 8c0 3.535 2.292 6.533 5.47 7.59.4.075.547-.172.547-.385 0-.19-.007-.693-.01-1.36-2.226.483-2.695-1.073-2.695-1.073-.364-.924-.89-1.17-.89-1.17-.725-.496.056-.486.056-.486.803.056 1.225.824 1.225.824.714 1.223 1.873.87 2.33.665.072-.517.278-.87.507-1.07-1.777-.2-3.644-.888-3.644-3.953 0-.873.31-1.587.823-2.147-.083-.202-.358-1.015.077-2.117 0 0 .672-.215 2.2.82.638-.178 1.323-.266 2.003-.27.68.004 1.364.092 2.003.27 1.527-1.035 2.198-.82 2.198-.82.437 1.102.163 1.915.08 2.117.513.56.823 1.274.823 2.147 0 3.073-1.87 3.75-3.653 3.947.287.246.543.735.543 1.48 0 1.07-.01 1.933-.01 2.195 0 .215.144.463.55.385C13.71 14.53 16 11.534 16 8c0-4.418-3.582-8-8-8" /></svg>
             <span class="f6 db">GitHub</span>
@@ -37,6 +46,7 @@ const ContactMe = () => {
             <span class="f6 db">Code Pen</span>
         </a>
     </footer>
+        </React.Fragment>
     );
 }
 
