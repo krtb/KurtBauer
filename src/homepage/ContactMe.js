89aad766547a12a999058c1169b598f1c4e6ef6e
Kurt Bauer <kurtbauer55@protonmail.com>
2019-05-31 17:00:45 -0400

    center email text

--- src/homepage/ContactMe.js
+++ src/homepage/ContactMe.js
@@ -5,12 +5,12 @@ const ContactMe = () => {
     return(
             <React.Fragment>
             <header class="tc ph4 sans-serif">
-                <h1 class="f3 f2-m f1-l fw2 black-90 mv3">
+                <h1 class="f5 f4-m f3-l fw2 black-90 mt0 lh-copy">
                     kurtbauer55@protonmail.com
-  </h1>
+                </h1>
                 <h2 class="f5 f4-m f3-l fw2 black-50 mt0 lh-copy">
-                    Let's talk
-  </h2>
+                    let's talk
+                </h2>
             </header>
             <footer class="pv4 ph3 tc sans-serif">
             {/* <div class="ph3 ph4-ns pv6 bt b--black-10 black-70">
