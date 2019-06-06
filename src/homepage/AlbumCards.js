09c5839e07cbe9d2f294dad0cd8639a69da620cb
Kurt Bauer <kurtbauer55@protonmail.com>
2019-06-05 23:29:39 -0400

    import imgs, add display cards with links to sites

--- src/homepage/AlbumCards.js
+++ src/homepage/AlbumCards.js
@@ -1,5 +1,8 @@
 import FabrikantImg from '../common/images/fabrikantpage.jpg'
 import YoutubeImg from '../common/images/youtubepage.png'
+import Eimg from '../common/images/enewspage.png'
+import Cryptimg from '../common/images/crypto_crashpage.png'
+import Informee from '../common/images/InforMeepage.png'
 
 import React from 'react';
 
@@ -13,25 +16,60 @@ const AlbumCards = () => {
                         class="db bg-center cover aspect-ratio--object" />
                 </div>
                 <a href="#0" class="ph2 ph0-ns pb3 link db">
-                    <h3 class="f5 f4-ns mb0 black-90">Fabrikant</h3>
+                    <h3 class="f5 f4-ns mb0 black-90">Fabrikant Jewlers</h3>
                     <h3 class="f6 f5 fw4 mt2 black-60">Client Catalog Page</h3>
                 </a>
             </article>
             </a>
-            <a target="_blank" href="http://fabrikant.eachscape.com/">
+            <a target="_blank" href="https://youtubereactclone.netlify.com">
                 <article class="fl w-100 w-50-m  w-25-ns pa2-ns">
                     <div class="aspect-ratio aspect-ratio--1x1">
                         <img src={YoutubeImg}
                             class="db bg-center cover aspect-ratio--object" />
                     </div>
                     <a href="#0" class="ph2 ph0-ns pb3 link db">
-                        <h3 class="f5 f4-ns mb0 black-90">Fabrikant</h3>
-                        <h3 class="f6 f5 fw4 mt2 black-60">Client Catalog Page</h3>
+                        <h3 class="f5 f4-ns mb0 black-90">Youtube Clone</h3>
+                        <h3 class="f6 f5 fw4 mt2 black-60">Youtube API, React.js</h3>
+                    </a>
+                </article>
+            </a>
+            <a target="_blank" href="http://fabrikant.eachscape.com/">
+                <article class="fl w-100 w-50-m  w-25-ns pa2-ns">
+                    <div class="aspect-ratio aspect-ratio--1x1">
+                        <img src={Eimg}
+                            class="db bg-center cover aspect-ratio--object" />
+                    </div>
+                    <a href="#0" class="ph2 ph0-ns pb3 link db">
+                        <h3 class="f5 f4-ns mb0 black-90">E! News for Android/iOS</h3>
+                        <h3 class="f6 f5 fw4 mt2 black-60">React-Native Mobile App</h3>
+                    </a>
+                </article>
+            </a>
+            <a target="_blank" href="http://fabrikant.eachscape.com/">
+                <article class="fl w-100 w-50-m  w-25-ns pa2-ns">
+                    <div class="aspect-ratio aspect-ratio--1x1">
+                        <img src={Cryptimg}
+                            class="db bg-center cover aspect-ratio--object" />
+                    </div>
+                    <a href="#0" class="ph2 ph0-ns pb3 link db">
+                        <h3 class="f5 f4-ns mb0 black-90">Crypto_Crash</h3>
+                        <h3 class="f6 f5 fw4 mt2 black-60">Ruby app</h3>
+                    </a>
+                </article>
+            </a>
+            <a target="_blank" href="http://fabrikant.eachscape.com/">
+                <article class="fl w-100 w-50-m  w-25-ns pa2-ns">
+                    <div class="aspect-ratio aspect-ratio--1x1">
+                        <img src={Informee}
+                            class="db bg-center cover aspect-ratio--object" />
+                    </div>
+                    <a href="#0" class="ph2 ph0-ns pb3 link db">
+                        <h3 class="f5 f4-ns mb0 black-90">InforMee</h3>
+                        <h3 class="f6 f5 fw4 mt2 black-60">React, Rails</h3>
                     </a>
                 </article>
             </a>
         </section>
-
     );
 };
 
