b5a41f776d619e2126a82d202b722faf0110bc8e
Kurt Bauer <kurtbauer55@protonmail.com>
2019-10-21 12:45:50 -0400

    add new path for pdf file, after moving assets folder

--- src/common/components/nav.js
+++ src/common/components/nav.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Logo from '../../common/images/kurtOct2018.jpg'
-import PDF from '../../../assets/docs/KurtBauer.8.20.19.pdf'
+import PDF from '../../assets/docs/KurtBauer.8.20.19.pdf'
 
 export default () => (
   <nav className="db dt-l w-100 border-box pa3 ph5-l">
