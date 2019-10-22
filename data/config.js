3fa32d8866d03fc532607fb04f8c2b61653f21be
Kurt Bauer <kurtbauer55@protonmail.com>
2019-10-22 12:30:13 -0400

    add files to source tracking

---
+++ data/config.js
@@ -0,0 +1,35 @@
+module.exports = {
+	defaultTitle: 'Kurt Bauer',
+	logo: '',
+	author: 'Kurt Bauer',
+	url: '',
+	legalName: 'Kurt Bauer',
+	defaultDescription: 'I’m Kurt, and I’m a Full-Stack Engineer & Mobile Developer',
+	socialLinks: {
+		twitter: 'https://twitter.com/kurtbauer_',
+		github: 'https://github.com/krtb',
+		linkedin: 'https://www.linkedin.com/in/kurt-bauer/',
+		instagram: '',
+		youtube: '',
+		google: '',
+	},
+	googleAnalyticsID: '',
+	themeColor: '#6b63ff',
+	backgroundColor: '#6b63ff',
+	social: {
+		facebook: 'appId',
+		twitter: '@kurtbauer_',
+	},
+	address: {
+		city: 'Brooklyn',
+		region: 'NY',
+		country: 'USA',
+		zipCode: '',
+	},
+	contact: {
+		email: 'kurt.bauer55@gmail.com',
+		phone: '954-547-5395',
+	},
+	foundingDate: '2018',
+	recaptcha_key: ''
+}
